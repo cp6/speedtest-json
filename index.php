8562120f84f6d9e24758bf5c3d7886e1131bd7d3
cp6 <cp6@users.noreply.github.com>
2023-11-14 16:41:54 +1100

    Updated examples

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Corbpie\NetworkSpeed;
 
-//$id = "1695921844_9S0EAS_ASIA";//2023.09.22 ASIA
-//$id = "adsdgdghfdhfhj";//HTTP 404
-$id = "1699510871_9M4L81_GLOBAL";//2023.10.24 GLOBAL
-//$id = "1699555358_08KGSH_INDIA";//2023.10.24 India
-//$id = "1695105636_FCDSD4_GLOBAL";//2023.09.04 GLOBAL
-//$id = "1693490247_21NFLN_NA";//2023.08.28 NORTH AMERICA
+//$filename = "1695921844_9S0EAS_ASIA";//2023.09.22 ASIA
+//$filename = "adsdgdghfdhfhj";//HTTP 404
+$filename = "1699510871_9M4L81_GLOBAL";//2023.10.24 GLOBAL
+//$filename = "1699555358_08KGSH_INDIA";//2023.10.24 India
+//$filename = "1695105636_FCDSD4_GLOBAL";//2023.09.04 GLOBAL
+//$filename = "1693490247_21NFLN_NA";//2023.08.28 NORTH AMERICA
 
-$ns = new NetworkSpeed\NetworkSpeed($id);
+$ns = new NetworkSpeed\NetworkSpeed($filename);
 
 echo $ns->outputAsJson();

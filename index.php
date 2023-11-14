7ac9aaa7663f5d13fa3e941ee67b402c7b7ee119
cp6 <cp6@users.noreply.github.com>
2023-11-14 22:56:36 +1100

    Added example

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 use Corbpie\NetworkSpeed;
 
 //$filename = "1695921844_9S0EAS_ASIA";//2023.09.22 ASIA
-//$filename = "adsdgdghfdhfhj";//HTTP 404
-$filename = "1699510871_9M4L81_GLOBAL";//2023.10.24 GLOBAL
-//$filename = "1699555358_08KGSH_INDIA";//2023.10.24 India
+//$filename = "adsdgdghfdhfhj";//HTTP 404 code
+//$filename = "1699510871_9M4L81_GLOBAL";//2023.10.24 GLOBAL
+$filename = "1699940146_TXQDAS_NA";//2023.11.13 NORTH AMERICA
+//$filename = "1699555358_08KGSH_INDIA";//2023.10.24 INDIA
 //$filename = "1695105636_FCDSD4_GLOBAL";//2023.09.04 GLOBAL
 //$filename = "1693490247_21NFLN_NA";//2023.08.28 NORTH AMERICA
 
